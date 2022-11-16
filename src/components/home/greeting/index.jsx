bff94ecc481254446ce4837575da60437b77d55d
TharinduDakshina <tharindudakshina527@gmail.com>
2022-11-16 12:43:12 +0530

    frunt page compleat

--- src/components/home/greeting/index.jsx
+++ src/components/home/greeting/index.jsx
@@ -1,19 +1,21 @@
 import {Component} from "react";
 import {styleSheet} from "./styles"
 import {withStyles} from "@mui/styles";
-import {Button, Typography} from "@mui/material";
+import {Button, Card, CardActions, CardContent, Typography} from "@mui/material";
+import ButtonBase from '@mui/material/ButtonBase';
+import {styled} from '@mui/material/styles';
+import image1 from "../../../assets/images/supermarker1.jpg"
 
 
-class Greeting extends Component{
+class Greeting extends Component {
 
     constructor(props) {
         super(props);
     }
 
     render() {
-        let {classes}=this.props;
-        return(
-            <div className={classes.container}>
+        let {classes} = this.props;
+        return (<div className={classes.container}>
                 <div className={classes.title__container}>
                     <div className={classes.title__container_characters}>
                         <Typography variant="h3" gutterBottom>
@@ -21,19 +23,23 @@ class Greeting extends Component{
                         </Typography>
                     </div>
                     <div className={classes.title__container_characters}>
-                        <Button variant="text" style={{marginRight:"15%",color:"black",fontSize:"1.5em"}}>Customer</Button>
+                        <Button variant="text"
+                                style={{marginRight: "15%", color: "black", fontSize: "1.5em"}}>Customer</Button>
 
-                        <Button variant="text" style={{color:"black",fontSize:"1.5em"}}>Item</Button>
+                        <Button variant="text" style={{color: "black", fontSize: "1.5em"}}>Item</Button>
 
                     </div>
 
                 </div>
 
                 <div className={classes.contentBody}>
-
+                    <Card className={classes.card}>
+                        <CardContent className={classes.content__card1}>
+                            <img src={image1} alt="sdas" className={classes.card_image}/>
+                        </CardContent>
+                    </Card>
                 </div>
-            </div>
-        );
+            </div>);
     }
 }
 
--- src/components/home/greeting/styles.js
+++ src/components/home/greeting/styles.js
@@ -2,24 +2,48 @@ export const styleSheet={
     container:{
         display:"flex",
         flexDirection:"column ",  //column  column-reverse  row row-reverse
-        width:"98.5vw",
-        height:"98.4vh",
-        flexWrap:"wrap"
+        width:"97.5vw",
+        height:"97.3vh",
+        flexWrap:"wrap",
+        overflow:"hidden"
     },
     title__container:{
         display: "flex",
         flexDirection: "row",
         alignItems:"center",
-        height:"10%"
-
+        height:"10%",
+        backgroundColor:"#Bebac1",
+        borderRadius:"20px"
     },
     title__container_characters:{
         width: "50%",
         display: "flex",
-        flexDirection: "row"
+        flexDirection: "row",
+
     },
     contentBody:{
+        width:"100%",
         height:"90%",
-        backgroundColor:"yellow"
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    card:{
+        maxWidth:"100%",
+        maxHeight:"100%"
+    },
+    content__card1:{
+        height:"100%",
+        display:"flex",
+        flexDirection:"row",
+        alignItems: "center"
+    },
+    card_image:{
+        backgroundSize: 'cover',
+        backgroundPosition: 'center 40%',
+        width: '100vw',
+        opacity: 0.83,
+        borderRadius:"20px",
+        height:"100vh"
     }
 }
